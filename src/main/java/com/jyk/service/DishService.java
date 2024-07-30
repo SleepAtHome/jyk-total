f8cc4245e05d526ea26e8bf49b6ae35214c6bc45
Jing yk <773669388@qq.com>
2024-07-30 22:38:35 +0800

    perf: 增加备注

--- src/main/java/com/jyk/service/DishService.java
+++ src/main/java/com/jyk/service/DishService.java
@@ -11,8 +11,24 @@ import java.util.List;
  * @date : Created in 2024/7/27 23:49
  */
 public interface DishService {
+    /**
+     * 获取所有菜品
+     * @return
+     */
     ResponseVo<List<Dish>> selectAllDishes();
+
+    /**
+     * 增加一个菜品
+     * @param dish
+     * @return
+     */
     ResponseVo<Integer> insertOneDish(Dish dish);
+
+    /**
+     * 根据菜名获取菜品
+     * @param dishName
+     * @return
+     */
     ResponseVo<List<Dish>> getDishByName(String dishName);
 
     /*int updateUser(User user);

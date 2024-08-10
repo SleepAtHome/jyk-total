ca370e83e1356ff4d1bf6a496972721228dde217
Jing yk <773669388@qq.com>
2024-08-11 03:09:32 +0800

    feat: 前后端数据交互加密（RSA非对称加密）

--- src/main/java/com/jyk/service/impl/UserServiceImpl.java
+++ src/main/java/com/jyk/service/impl/UserServiceImpl.java
@@ -4,14 +4,12 @@ import com.jyk.dao.User;
 import com.jyk.enums.ResponseEnum;
 import com.jyk.mapper.UserMapper;
 import com.jyk.service.UserService;
+import com.jyk.utils.RsaUtils;
 import com.jyk.vo.ResponseVo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import javax.xml.crypto.Data;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -22,6 +20,9 @@ import java.util.Objects;
  */
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Value("${rsa.private.key}")
+    String privateKey;
 
     @Autowired
     UserMapper userMapper;
@@ -38,13 +39,24 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseVo<Boolean> login(User user) {
-        String inputAccount = user.getAccount();
-        String inputPassword = user.getPassword();
-
-        if (StringUtils.isEmpty(inputAccount) || StringUtils.isEmpty(inputPassword)) {
+        // 判断入参
+        if (null==user || StringUtils.isEmpty(user.getAccount()) || StringUtils.isEmpty(user.getPassword())) {
             System.out.println("登录失败，账号或密码为空");
             return ResponseVo.error(ResponseEnum.LOGIN_ERROR);
         }
+
+        String inputAccount = user.getAccount();
+        // RSA私钥解密
+        String inputPassword = null;
+        try {
+            inputPassword = RsaUtils.decryptByPrivateKey(privateKey, user.getPassword());
+        } catch (Exception e) {
+            System.out.println("解密密码失败！");
+            e.printStackTrace();
+            //throw new RuntimeException(e);
+            return ResponseVo.error(ResponseEnum.LOGIN_ERROR);
+        }
+
 
         // 查询用户数据
         User dataU = userMapper.selectUserByAccount(inputAccount);
@@ -53,6 +65,17 @@ public class UserServiceImpl implements UserService {
         if (Objects.isNull(dataU) || !inputPassword.equals(dataU.getPassword())) {
             return ResponseVo.error(ResponseEnum.LOGIN_ERROR);
         }
+
+        /* 登录态
+        String token = TokenUtil.generateToken(user.getUsername());
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(one.getId());
+        userInfo.setUsername(user.getUsername());
+        userInfo.setToken(token);
+        String sessionId = TokenUtil.storeTokenInSession(request, token);
+        userInfo.setSessionId(sessionId);
+        return R.ok(userInfo);*/
+
 
         return ResponseVo.success(true);
     }
---
+++ src/main/java/com/jyk/utils/RsaUtils.java
@@ -0,0 +1,187 @@
+package com.jyk.utils;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.ObjectUtils;
+
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+/**
+ * @author : Jing Yuankui
+ * @description : RSA非对称加密工具类，用于生成公钥/私钥 并且进行验证
+ * @date : Created in 2024/8/11 1:47
+ */
+public class RsaUtils {
+
+    private static final String NEED_ENC_STR = "jykok";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n");
+
+        // 加解密
+        RsaKeyPair keyPair = generateKeyPair();
+        System.out.println("公钥：" + keyPair.getPublicKey());
+        System.out.println("私钥：" + keyPair.getPrivateKey());
+        System.out.println("\n");
+
+        // 验证
+        publicEncPrivateDec(keyPair);
+        System.out.println("\n");
+        privateEncPublicDec(keyPair);
+        System.out.println("\n");
+
+    }
+
+    /**
+     * 构建RSA密钥对
+     *
+     * @return /
+     * @throws NoSuchAlgorithmException /
+     */
+    public static RsaKeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        // 密钥生成器  使用RSA算法
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(1024);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
+        String publicKeyString = Base64.encodeBase64String(rsaPublicKey.getEncoded());
+        String privateKeyString = Base64.encodeBase64String(rsaPrivateKey.getEncoded());
+        return new RsaKeyPair(publicKeyString, privateKeyString);
+    }
+
+    /**
+     * 公钥加密私钥解密
+     */
+    private static void publicEncPrivateDec(RsaKeyPair keyPair) throws Exception {
+        System.out.println("***************** 公钥加密私钥解密开始 *****************");
+        String text1 = encryptByPublicKey(keyPair.getPublicKey(), RsaUtils.NEED_ENC_STR);
+        String text2 = decryptByPrivateKey(keyPair.getPrivateKey(), text1);
+        System.out.println("加密前：" + RsaUtils.NEED_ENC_STR);
+        System.out.println("加密后：" + text1);
+        System.out.println("解密后：" + text2);
+        if (RsaUtils.NEED_ENC_STR.equals(text2)) {
+            System.out.println("解密字符串和原始字符串一致，解密成功");
+        } else {
+            System.out.println("解密字符串和原始字符串不一致，解密失败");
+        }
+        System.out.println("***************** 公钥加密私钥解密结束 *****************");
+    }
+
+    /**
+     * 公钥加密
+     *
+     * @param publicKeyText 公钥
+     * @param text 待加密的文本
+     * @return /
+     */
+    public static String encryptByPublicKey(String publicKeyText, String text) throws Exception {
+        X509EncodedKeySpec x509EncodedKeySpec2 = new X509EncodedKeySpec(Base64.decodeBase64(publicKeyText));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec2);
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] result = cipher.doFinal(text.getBytes());
+        return Base64.encodeBase64String(result);
+    }
+
+    /**
+     * 私钥解密
+     *
+     * @param privateKeyText 私钥
+     * @param text 待解密的文本
+     * @return /
+     * @throws Exception /
+     */
+    public static String decryptByPrivateKey(String privateKeyText, String text) throws Exception {
+        if (ObjectUtils.isEmpty(text)) {
+            return null;
+        }
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec5 = new PKCS8EncodedKeySpec(Base64.decodeBase64(privateKeyText));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec5);
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] result = cipher.doFinal(Base64.decodeBase64(text));
+        return new String(result);
+    }
+
+    /**
+     * 私钥加密公钥解密
+     * @throws Exception /
+     */
+    private static void privateEncPublicDec(RsaKeyPair keyPair) throws Exception {
+        System.out.println("***************** 私钥加密公钥解密开始 *****************");
+        String text1 = encryptByPrivateKey(keyPair.getPrivateKey(), RsaUtils.NEED_ENC_STR);
+        String text2 = decryptByPublicKey(keyPair.getPublicKey(), text1);
+        System.out.println("加密前：" + RsaUtils.NEED_ENC_STR);
+        System.out.println("加密后：" + text1);
+        System.out.println("解密后：" + text2);
+        if (RsaUtils.NEED_ENC_STR.equals(text2)) {
+            System.out.println("解密字符串和原始字符串一致，解密成功");
+        } else {
+            System.out.println("解密字符串和原始字符串不一致，解密失败");
+        }
+        System.out.println("***************** 私钥加密公钥解密结束 *****************");
+    }
+
+    /**
+     * 私钥加密
+     *
+     * @param privateKeyText 私钥
+     * @param text 待加密的信息
+     * @return /
+     * @throws Exception /
+     */
+    public static String encryptByPrivateKey(String privateKeyText, String text) throws Exception {
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(privateKeyText));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        byte[] result = cipher.doFinal(text.getBytes());
+        return Base64.encodeBase64String(result);
+    }
+
+    /**
+     * 公钥解密
+     *
+     * @param publicKeyText 公钥
+     * @param text 待解密的信息
+     * @return /
+     * @throws Exception /
+     */
+    public static String decryptByPublicKey(String publicKeyText, String text) throws Exception {
+        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(Base64.decodeBase64(publicKeyText));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        byte[] result = cipher.doFinal(Base64.decodeBase64(text));
+        return new String(result);
+    }
+
+
+    /**
+     * RSA密钥对对象
+     */
+    public static class RsaKeyPair {
+        private final String publicKey;
+        private final String privateKey;
+        public RsaKeyPair(String publicKey, String privateKey) {
+            this.publicKey = publicKey;
+            this.privateKey = privateKey;
+        }
+        public String getPublicKey() {
+            return publicKey;
+        }
+        public String getPrivateKey() {
+            return privateKey;
+        }
+    }
+}
+
